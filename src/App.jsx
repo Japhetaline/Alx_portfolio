7fd5ba9cb3a6e6846d613baed9807d9715b336f6
Georgeadesua <georgeadesua3@gmail.com>
2023-10-27 07:59:39 +0100

    portfolio

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandPage from './LandPage';
+import About from './AboutPage/About'
+import Login from './Login/Login'; 
+import FAQ from './FaqPage/faq';
+import RESET from './resetpasswordPage/reset';
+import SIGNUP from './Signup/signup'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LandPage />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/faq" element={<FAQ />} />
+      <Route path="/reset" element={<RESET />} />
+      <Route path="/signup" element={<SIGNUP />} />
+      {/* Define other routes here */}
+    </Routes>
+  </BrowserRouter>
+  )
+}
---
+++ src/LandPage.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Navbar from './Navbar/Navbar';
+import './LandPage.css'
+
+export default function LandPage() {
+    const navigate = useNavigate(); // Use useNavigate hook
+
+  const handleGetStarted = () => {
+    navigate('/signup'); // Navigate to the '/signup' page
+  };
+
+  const handleLearnMore = () => {
+    navigate('/faq'); // Navigate to the '/faq' page
+  };
+  return (
+    <>
+    <Navbar/>
+
+        <p className='content-text'>
+        The page should contain sections/headings for: Welcome to Journaly, with some text under; How Journaly works, with some text under; and How to use Journaly, with some text under
+After the above sections, there should be a Get Started Now button that leads to the Login/Signup page
+      </p>
+      <div className="content-btn">
+      <button className="get-started-button" onClick={handleGetStarted}>
+        Get Started
+      </button>
+      <button className="learn-more-button" onClick={handleLearnMore}>
+        Learn More
+      </button>
+      </div>
+      
+        <div className="footer-content">
+        <h1 className="Footer-text">Do Hard Things</h1>
+        </div>
+    </>
+  
+  )
+}
